34621d8ca5861cf2d5060eb2aa693e1ea42f32d8
Kristóf Szőllősi <szkristof2h@gmail.com>
2023-12-10 16:48:31 +0100

    add some initial styling

--- src/app/post.tsx
+++ src/app/post.tsx
@@ -13,11 +13,11 @@ export default function Post(props: Props) {
   const { id, name, date, text, postedByName, postedByLink } = props
 
   return (
-    <div className="h-64 w-64 overflow-hidden rounded bg-white p-4">
+    <div className="backdrop-blur-m h-64 w-64 overflow-hidden rounded-lg bg-white/30 p-4 text-slate-600 drop-shadow-lg backdrop-hue-rotate-15">
       <h1>{name}</h1>
       <div>{date.toDateString()}</div>
       <Link href={postedByLink}>{postedByName}</Link>
-      <div className="text-ellipsis">{text}</div>
+      <div className="h-40 overflow-hidden">{text}</div>
     </div>
   )
 }
