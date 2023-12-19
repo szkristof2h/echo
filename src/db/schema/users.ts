54f9cb67ce6668a669603b172a413f7f2c4fbb40
Kristóf Szőllősi <szkristof2h@gmail.com>
2023-12-19 21:16:41 +0100

    make user bio optional

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -3,7 +3,7 @@ import { pgTable, serial, varchar } from "drizzle-orm/pg-core"
 export const users = pgTable("users", {
   id: serial("id").primaryKey().notNull(),
   displayName: varchar("display_name", { length: 16 }).notNull(),
-  bio: varchar("bio", { length: 256 }).notNull(),
+  bio: varchar("bio", { length: 256 }),
 })
 
 export type User = typeof users.$inferSelect // return type when queried
