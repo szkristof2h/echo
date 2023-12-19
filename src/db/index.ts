63a744d6aec8df4015def060f8f8ba82cb298c14
Kristóf Szőllősi <szkristof2h@gmail.com>
2023-12-19 21:17:41 +0100

    fix schema & typing

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/vercel-postgres"
 import { sql } from "@vercel/postgres"
+import schema from "./schema"
 
-const db = drizzle(sql)
+const db = drizzle(sql, { schema })
 
 export default db
