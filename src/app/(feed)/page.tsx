eea0184a5f0ab05b36d8c3a8f062d24cd8f144eb
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-02-06 20:01:44 +0100

    remove hardcoded data

--- src/app/(feed)/page.tsx
+++ src/app/(feed)/page.tsx
@@ -1,21 +1,12 @@
 import Head from "next/head"
 import Posts from "./components/posts"
+import { getEchos } from "~/data/echos"
 
-const text =
-  "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+export default async function Home() {
+  const echos = await getEchos()
 
-const posts = [
-  {
-    id: "234",
-    title: "ITS A ME MARIO",
-    date: new Date(),
-    text: text,
-    postedByName: "Bela",
-    postedByLink: "https://tulipsarebetter.com/user/bela2",
-  },
-]
+  const { posts } = echos
 
-export default function Home() {
   return (
     <>
       <Head>
