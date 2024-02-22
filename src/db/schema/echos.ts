6d8911b98cccd0354b3db2c79278f0d91315782d
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-02-22 18:23:07 +0100

    update echo schema

--- src/db/schema/echos.ts
+++ src/db/schema/echos.ts
@@ -1,12 +1,20 @@
-import { pgTable, serial, integer, varchar } from "drizzle-orm/pg-core"
-import { relations } from "drizzle-orm"
-import { users } from "./users"
+import {
+  pgTable,
+  serial,
+  integer,
+  varchar,
+  timestamp,
+} from "drizzle-orm/pg-core"
+import { relations, sql } from "drizzle-orm"
+import { User, users } from "./users"
 
 export const echos = pgTable("echos", {
   id: serial("id").primaryKey().notNull(),
   idSender: integer("idSender").notNull(),
   idUser: integer("idUser").notNull(),
   text: varchar("display_name", { length: 512 }).notNull(),
+  title: varchar("title", { length: 64 }).notNull(),
+  date: timestamp("timestamp4").default(sql`now()`),
 })
 
 export const echosRelations = relations(echos, ({ one }) => ({
@@ -16,3 +24,8 @@ export const echosRelations = relations(echos, ({ one }) => ({
 
 export type Echo = typeof echos.$inferSelect // return type when queried
 export type NewEcho = typeof echos.$inferInsert // insert type
+
+export type EchoWithUser = Omit<Echo, "idSender" | "idUser"> & {
+  postedBy: Omit<User, "bio">
+  echodTo: Omit<User, "bio">
+}
