d99fc9fba45cb884ef53b7b3c1783ec7f4143fa4
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-02-23 19:47:35 +0100

    add links

--- src/app/(feed)/components/post.tsx
+++ src/app/(feed)/components/post.tsx
@@ -7,17 +7,21 @@ type Props = EchoWithUser
 
 export default function Post(props: Props) {
   const { id, title, date, text, postedBy, echodTo } = props
-  const url = `/profile${postedBy.id}`
+  const echoUrl = `/echo/${id}`
+  const postedByUrl = `/profile/${postedBy.id}`
+  const echodToUrl = `/profile/${echodTo.id}`
 
   return (
     <Container>
-      <h1 className="text-xl">{title}</h1>
+      <h1 className="text-xl">
+        <Link href={echoUrl}>{title}</Link>
+      </h1>
       <div className="max-h-40 overflow-hidden">{text}</div>
       <div className="flex justify-between text-sm">
         <span>
-          <Link href={url}>{postedBy.displayName}</Link>
+          <Link href={postedByUrl}>{postedBy.displayName}</Link>
           {" > "}
-          <Link href={url}>{echodTo.displayName}</Link>
+          <Link href={echodToUrl}>{echodTo.displayName}</Link>
         </span>
         <span>{new Date(date ?? "").toDateString()}</span>
       </div>
