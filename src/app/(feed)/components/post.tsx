d857df106dcb8916c1289798554ae63a62326319
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-02-23 22:10:36 +0100

    style echo title

--- src/app/(feed)/components/post.tsx
+++ src/app/(feed)/components/post.tsx
@@ -13,7 +13,7 @@ export default function Post(props: Props) {
 
   return (
     <Container>
-      <h1 className="text-xl">
+      <h1 className="mb-3 text-xl">
         <Link href={echoUrl}>{title}</Link>
       </h1>
       <div className="max-h-40 overflow-hidden">{text}</div>
