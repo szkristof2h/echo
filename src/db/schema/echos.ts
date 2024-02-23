8fe335b7ec2462d8721ccee0776dca4c74082fef
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-02-23 17:15:21 +0100

    rename db field

--- src/db/schema/echos.ts
+++ src/db/schema/echos.ts
@@ -14,7 +14,7 @@ export const echos = pgTable("echos", {
   idUser: integer("idUser").notNull(),
   text: varchar("display_name", { length: 512 }).notNull(),
   title: varchar("title", { length: 64 }).notNull(),
-  date: timestamp("timestamp").default(sql`now()`),
+  date: timestamp("date").default(sql`now()`),
 })
 
 export const echosRelations = relations(echos, ({ one }) => ({
