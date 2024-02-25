99c3c130aa410955d9eaf816171e28855e238ad6
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-02-25 13:14:01 +0100

    add endpoint for searching users

---
+++ src/app/api/user/[name]/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server"
+import { ilike } from "drizzle-orm"
+import { users } from "~/db/schema/users"
+import db from "~/db"
+
+export async function GET(
+  request: Request,
+  context: { params: { name?: string } },
+) {
+  const name = context.params.name
+
+  if (!name || name.length < 3) return null
+
+  const matchedUsers = await db
+    .select({
+      id: users.id,
+      displayName: users.displayName,
+    })
+    .from(users)
+    .where(ilike(users.displayName, `%${name}%`))
+    .limit(5)
+
+  return NextResponse.json({ users: matchedUsers }, { status: 200 })
+}
