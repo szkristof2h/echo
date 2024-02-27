c268d0a71803888c1591bfe3b65e4223601dac42
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-02-28 00:44:07 +0100

    add echo form

---
+++ src/app/echoTo/[[...id]]/components/echo-form.tsx
@@ -0,0 +1,25 @@
+import { ActionResponse, createEcho } from "~/app/lib/actions"
+import { SubmitButton } from "./submit-button"
+import { useFormState } from "react-dom"
+
+export default async function EchoForm(props: { id?: number }) {
+  const { id } = props
+  const handleSubmit = (prevState: ActionResponse, formData: FormData) =>
+    createEcho(formData)
+  const [state, formAction] = useFormState(handleSubmit, {
+    status: "",
+    idUser: id,
+  })
+
+  console.log("state")
+  console.log(state)
+
+  return state.status === "success" ? (
+    <p>Echo posted successfully!</p>
+  ) : (
+    <form action={formAction} className="flex flex-col gap-y-2">
+      <textarea name="text" autoFocus className="outline-none" />
+      <SubmitButton text="Submit" />
+    </form>
+  )
+}
