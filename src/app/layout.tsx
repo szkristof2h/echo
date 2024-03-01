57fc14be6d9f1b40b97b7aaf7e5999bcaf632ac9
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-03-01 23:24:40 +0100

    add basic navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <nav className="absolute right-2">
+          <Link href="/echoTo">
+            <button className="mx-auto mr-4 mt-4 w-32 bg-teal-400 py-2 text-white hover:bg-teal-600">
+              ⭐️ ECHO ⭐️
+            </button>
+          </Link>
+          <Link href="/profile">
+            <button className="mx-auto mr-4 mt-4 bg-teal-400 px-4 py-2 text-white hover:bg-teal-600">
+              🧒
+            </button>
+          </Link>
+          <Link href="/settings">
+            <button className="mx-auto mt-4 bg-teal-400 px-4 py-2 text-white hover:bg-teal-600">
+              ⚙️
+            </button>
+          </Link>
+        </nav>
         <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#00BFB2] to-[#f0f3bd] bg-fixed text-slate-600">
           <Link href="/">
             <h1 className="mt-8 font-mono text-7xl subpixel-antialiased drop-shadow-lg">
