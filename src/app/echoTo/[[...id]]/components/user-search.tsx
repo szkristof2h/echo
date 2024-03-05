ff133ef16510b71ad6394f1c171914c3b4d2320c
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-03-06 00:11:40 +0100

    add temp eslint disable

--- src/app/echoTo/[[...id]]/components/user-search.tsx
+++ src/app/echoTo/[[...id]]/components/user-search.tsx
@@ -40,6 +40,7 @@ export default function UserSearch(props: {
       }
     }
 
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
     if (!!inputUserName) handleOnChange()
 
     return () => {
