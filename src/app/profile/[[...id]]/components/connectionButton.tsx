5bcf74a6719723dc5cf44254e86fa19173a3fec9
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-03-05 11:07:26 +0100

    use dynamic user id

--- src/app/profile/[[...id]]/components/connectionButton.tsx
+++ src/app/profile/[[...id]]/components/connectionButton.tsx
@@ -2,14 +2,15 @@
 import { createConnection } from "~/app/lib/actions"
 
 export default async function ConnectionButton(props: {
+  idConnection: number
   hasConnection: boolean
   isPending: boolean
 }) {
-  const { hasConnection, isPending } = props
+  const { idConnection, hasConnection, isPending } = props
   return (
     <span
       className="ml-auto inline-block cursor-pointer hover:underline"
-      onClick={() => createConnection({ idUser: 1, idConnection: 2 })}
+      onClick={() => createConnection({ idUser: 1, idConnection })}
     >
       {hasConnection ? (isPending ? "sent" : "your connection") : "connect"}
     </span>
--- src/app/profile/[[...id]]/page.tsx
+++ src/app/profile/[[...id]]/page.tsx
@@ -15,11 +15,11 @@ export default async function Profile({
 }: {
   params: { id?: string[] }
 }) {
-  const id = params.id?.[0] ?? null
-  const user = id ? await getUser(parseInt(id, 10)) : await getCurrentUser()
+  const id = params.id?.[0] ? parseInt(params.id?.[0]) : null
+  const user = id ? await getUser(id) : await getCurrentUser()
   const displayName = user?.displayName ?? ""
   const bio = user?.bio ?? ""
-  const connection = await getConnection(2, 12)
+  const connection = id ? await getConnection(1, id) : null
   const hasConnection = !!connection?.date ?? false
   const isPending = connection?.isPending ?? false
 
@@ -30,16 +30,21 @@ export default async function Profile({
       <Container>
         <div className="flex border-b border-slate-600">
           <h2 className="inline-block">{displayName}</h2>
-          <ConnectionButton
-            hasConnection={hasConnection}
-            isPending={isPending}
-          />
-          <Link
-            className="ml-4 inline-block cursor-pointer hover:underline"
-            href={`/echoTo/${user?.id}`}
-          >
-            echo
-          </Link>
+          {id && (
+            <ConnectionButton
+              idConnection={id}
+              hasConnection={hasConnection}
+              isPending={isPending}
+            />
+          )}
+          {id && (
+            <Link
+              className="ml-4 inline-block cursor-pointer hover:underline"
+              href={`/echoTo/${user?.id}`}
+            >
+              echo
+            </Link>
+          )}
         </div>
         <div className="mb-4 border-b border-slate-600 py-4">{bio}</div>
         <div>
