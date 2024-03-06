bd55080bd8b12193ee42e63f25090efc18a7733d
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-03-06 19:12:44 +0100

    use User component

--- src/app/connections/page.tsx
+++ src/app/connections/page.tsx
@@ -2,6 +2,7 @@ import Container from "~/app/components/container"
 import Link from "next/link"
 import Head from "~/app/components/head"
 import { getConnections } from "~/data/connections"
+import User from "./components/user"
 
 export default async function Connections() {
   const connections = await getConnections(1)
@@ -18,23 +19,33 @@ export default async function Connections() {
       <Container>
         <h2>Pending connections</h2>
         <ul>
-          {pendingConnections?.map((connection) => (
-            <li key={connection.id}>
-              <Link href={`/profile/${connection.id}`}>
-                {connection.displayName}
-              </Link>
-            </li>
-          ))}
+          {pendingConnections?.map(
+            (connection) =>
+              connection.id &&
+              connection.displayName && (
+                <User
+                  id={connection.id}
+                  displayName={connection.displayName}
+                  idUser={connection.idUser}
+                  isPending={!!connection.isPending}
+                />
+              ),
+          )}
         </ul>
         <h2>Connections</h2>
         <ul>
-          {acceptedConnections?.map((connection) => (
-            <li key={connection.id}>
-              <Link href={`/profile/${connection.id}`}>
-                {connection.displayName}
-              </Link>
-            </li>
-          ))}
+          {acceptedConnections?.map(
+            (connection) =>
+              connection.id &&
+              connection.displayName && (
+                <User
+                  id={connection.id}
+                  displayName={connection.displayName}
+                  idUser={connection.idUser}
+                  isPending={false}
+                />
+              ),
+          )}
         </ul>
       </Container>
     </>
