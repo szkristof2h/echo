52a16cad6b642bde2bc5cf0dea4049c8b47eca31
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-03-09 23:44:38 +0100

    add linking to subecho

--- src/app/(feed)/components/comments.tsx
+++ src/app/(feed)/components/comments.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import { getEchos } from "~/data/echos"
 import FormWrapper from "./form-wrapper"
+import ProfileLink from "./profile-link"
 
 export default async function Comments(props: { id: number; idUser: number }) {
   const { id, idUser } = props
@@ -14,15 +15,18 @@ export default async function Comments(props: { id: number; idUser: number }) {
       <FormWrapper idUser={idUser} idParent={id} />
       <ul>
         {subEchos.map((subEcho) => (
-          <li
-            key={subEcho.id}
-            className="backdrop-blur-m ml-8 mt-4 overflow-hidden bg-white/80 p-4 drop-shadow-lg"
-          >
-            <Link href={`/profile${subEcho.echodTo.id}`}>
-              @{subEcho.echodTo.displayName}
-            </Link>{" "}
-            {subEcho.text}
-          </li>
+          <Link href={`/echo/${subEcho.id}`}>
+            <li
+              key={subEcho.id}
+              className="backdrop-blur-m ml-8 mt-4 overflow-hidden bg-white/80 p-4 drop-shadow-lg"
+            >
+              <ProfileLink
+                idUser={subEcho.echodTo.id}
+                displayName={subEcho.echodTo.displayName}
+              />{" "}
+              {subEcho.text}
+            </li>
+          </Link>
         ))}
       </ul>
     </>
