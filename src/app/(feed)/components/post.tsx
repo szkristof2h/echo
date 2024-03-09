09609b3e35005a15970a89d620b47996d118886f
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-03-10 00:07:20 +0100

    add reply to text to sub echos

--- src/app/(feed)/components/post.tsx
+++ src/app/(feed)/components/post.tsx
@@ -7,13 +7,16 @@ import ProfileLink from "./profile-link"
 type Props = EchoWithUser
 
 export default function Post(props: Props) {
-  const { id, title, date, text, postedBy, echodTo } = props
+  const { id, title, date, text, postedBy, echodTo, idParent } = props
   const echoUrl = `/echo/${id}`
 
   return (
     <Container>
       <h1 className="mb-3 text-xl">
-        <Link href={echoUrl}>{title}</Link>
+        <Link href={echoUrl}>
+          {idParent ? "Reply to: " : ""}
+          {title}
+        </Link>
       </h1>
       <div className="max-h-40 overflow-hidden">{text}</div>
       <div className="flex justify-between text-sm">
