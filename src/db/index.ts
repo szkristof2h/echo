017540e279e4ce91c597c446bc649400253405c6
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-03-15 17:11:17 +0100

    remove import

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,6 @@
 import { drizzle } from "drizzle-orm/vercel-postgres"
 import { sql } from "@vercel/postgres"
 import schema from "./schema"
-import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle"
 
 const db = drizzle(sql, { schema })
 
