89f80be6c0ee3b9fbaab501a60358fed9942298a
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-03-15 10:51:58 +0100

    add login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import Head from "~/app/components/head"
+import Container from "~/app/components/container"
+
+export default function Login() {
+  return (
+    <>
+      <Head title="Login" />
+      <Container>Login</Container>
+    </>
+  )
+}
