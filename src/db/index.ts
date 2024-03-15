f3359a22fe8553220167ddd3ee65f93c8e7f0e19
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-03-15 17:10:22 +0100

    add session table

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/vercel-postgres"
 import { sql } from "@vercel/postgres"
 import schema from "./schema"
+import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle"
 
 const db = drizzle(sql, { schema })
 
