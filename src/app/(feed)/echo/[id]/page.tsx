3c98359cb16ed594a460f7bc10fe4b94e62a55e2
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-03-22 21:03:28 +0100

    query user for post page

--- src/app/(feed)/echo/[id]/page.tsx
+++ src/app/(feed)/echo/[id]/page.tsx
@@ -2,6 +2,7 @@ import Head from "~/app/components/head"
 import { getEcho } from "~/data/echos"
 import Post from "../../components/post"
 import Comments from "../../components/comments"
+import { getUser } from "~/data/users"
 
 export default async function Home({ params }: { params: { id?: string } }) {
   const id = params?.id ? parseInt(params?.id, 10) : null
@@ -10,13 +11,19 @@ export default async function Home({ params }: { params: { id?: string } }) {
   if (!id) return <div>WWWW</div>
 
   const echo = await getEcho(id)
+  const postedBy = echo?.idSender ? await getUser(echo?.idSender) : null
+  const postedTo = echo?.idUser ? await getUser(echo?.idUser) : null
 
   return (
     <>
       <Head title={echo?.title ?? "..."} />
-      {!!echo ? <Post {...echo} /> : "Couldn't load echo"}
+      {!!echo ? (
+        <Post postedBy={postedBy} postedTo={postedTo} {...echo} />
+      ) : (
+        "Couldn't load echo"
+      )}
 
-      {echo && <Comments idUser={echo?.postedBy.id} id={id} />}
+      {echo && <Comments idUser={echo?.idSender} id={id} />}
     </>
   )
 }
