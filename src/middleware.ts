3a69ae1d026df9939fbf7784c4e63e58732dad61
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-03-26 21:03:11 +0100

    update public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs"
 
 export default authMiddleware({
-  publicRoutes: ["/", "/echo/:id", "/profile/:id", "/login"],
+  publicRoutes: ["/", "/echo/:id", "/profile/:id", "/welcome", "/goodbye"],
 })
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
