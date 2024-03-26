c22c9003f67a5c7b40aefa44627b4915a1a32b04
jenolah <olahjendev@gmail.com>
2024-03-26 13:04:28 +0100

    replace inputs with new input components

--- src/app/echoTo/[[...id]]/components/user-form.tsx
+++ src/app/echoTo/[[...id]]/components/user-form.tsx
@@ -1,6 +1,7 @@
 import type { ChangeEvent, Dispatch, SetStateAction } from "react"
 import { useDeferredValue, useState } from "react"
 import UserSearch from "./user-search"
+import { Input } from "~/app/components/input"
 
 export default function UserForm(props: {
   defaultName?: string | null
@@ -19,11 +20,10 @@ export default function UserForm(props: {
 
   return (
     <>
-      <input
+      <Input
         name="displayName"
         autoFocus
         placeholder="Echo to"
-        className="bg-emerald-50 px-4 py-2 text-slate-600 outline-none ring-inset ring-emerald-700 placeholder:text-slate-300 focus:ring-2"
         onChange={handleOnChange}
       />
       <UserSearch
