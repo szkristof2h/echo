b36318fdb4e70242d913822820e4e857f03a15a0
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-03-26 23:43:18 +0100

    show current topic on the front page

--- src/app/(feed)/page.tsx
+++ src/app/(feed)/page.tsx
@@ -1,13 +1,18 @@
 import Head from "~/app/components/head"
 import Posts from "./components/posts"
 import { getEchos } from "~/data/echos"
+import { getCurrentTopic } from "~/data/topics"
 
 export default async function Home() {
   const echos = await getEchos()
+  const topic = await getCurrentTopic()
 
   return (
     <>
       <Head title="Echo" />
+      <div className="bg-tertiary-dark mb-4 p-4">
+        This week's topic is {topic?.[0]?.text}
+      </div>
       <Posts posts={echos} />
     </>
   )
