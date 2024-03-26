d64b33cb1b76aa68cef95a5e10c8b53a8ef5bf5e
jenolah <olahjendev@gmail.com>
2024-03-26 21:08:12 +0100

    create color tokens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,20 @@ export default {
       spacing: {
         "128": "32rem",
       },
+      colors: {
+        primary: {
+          light: "rgba(236, 253, 245, 0.8)",
+          dark: "rgba(240, 260, 250, 0.8)",
+        },
+        secondary: {
+          light: "rgba(0, 163, 0, 1)",
+          dark: "rgba(30, 113, 69, 1)",
+        },
+        danger: {
+          light: "rgba(238,17,17,1)",
+          dark: "rgba(185,29,71,1)",
+        },
+      },
     },
   },
   plugins: [],
