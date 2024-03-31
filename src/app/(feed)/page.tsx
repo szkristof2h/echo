73f671c22b7f6ec11b98a63ee3459f801e028a28
jenolah <olahjendev@gmail.com>
2024-03-31 23:16:29 +0200

    increase margin

--- src/app/(feed)/page.tsx
+++ src/app/(feed)/page.tsx
@@ -16,7 +16,7 @@ export default async function Home() {
         This week's topic is {topic?.[0]?.text}
       </span>
       <Link href="/echoTo">
-        <Button className="mb-4 inline-block w-full p-4">Write Echo</Button>
+        <Button className="mb-8 inline-block w-full p-4">Write Echo</Button>
       </Link>
       <Posts posts={echos} />
     </>
