76bdc2b178e4e7fdd5f72d2b8cecfe7268545c47
jenolah <olahjendev@gmail.com>
2024-04-02 16:47:46 +0200

    🌈 rainbow button

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -3,17 +3,43 @@ import type { HTMLAttributes } from "react"
 interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
   buttonType?: "button" | "submit" | "reset"
   placeholder?: string
-  theme?: "primary" | "secondary" | "tertiary" | "danger" | "success"
+  theme?:
+    | "primary"
+    | "secondary"
+    | "tertiary"
+    | "danger"
+    | "success"
+    | "rainbow"
 }
 
 export const Button = (props: ButtonProps) => {
-  const { className, buttonType = "button", theme = "primary", ...rest } = props
+  const {
+    className,
+    buttonType = "button",
+    theme = "primary",
+    children,
+    ...rest
+  } = props
+
+  const isRainbow = theme === "rainbow"
+
+  const buttonTheme = isRainbow
+    ? "rainbow-border rounded-lg border-4"
+    : `button-${theme}`
 
   return (
     <button
       type={buttonType}
-      className={`button-${theme} ${className ?? ""}`}
+      className={`${buttonTheme} ${className ?? ""}`}
       {...rest}
-    />
+    >
+      {isRainbow ? (
+        <div className="bg-primary-light hover:bg-primary-dark rounded-md p-2 text-white">
+          {children}
+        </div>
+      ) : (
+        children
+      )}
+    </button>
   )
 }
