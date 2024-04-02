f66c36e1e264476a9739eb981d27804919c245c4
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-04-02 22:24:41 +0200

    use next's image component

--- src/app/components/icon.tsx
+++ src/app/components/icon.tsx
@@ -1,4 +1,5 @@
 import type { HTMLAttributes } from "react"
+import Image from "next/image"
 
 interface IconProps extends HTMLAttributes<HTMLImageElement> {
   iconName: string
@@ -9,10 +10,12 @@ export default function Icon(props: IconProps) {
   const { className, iconName, isDark = false, ...rest } = props
 
   return (
-    <img
+    <Image
       className={`h-6 ${isDark ? "invert-[0.25]" : "invert"} ${className}`}
       src={`/icons/${iconName}.svg`}
       alt={iconName}
+      width={24}
+      height={24}
       {...rest}
     />
   )
