032a2436f6ae3058e99a6fc07a0250b1b66e76a0
jenolah <olahjendev@gmail.com>
2024-04-02 18:02:08 +0200

    create icons ⚡️

---
+++ src/app/components/icon.tsx
@@ -0,0 +1,19 @@
+import type { HTMLAttributes } from "react"
+
+interface IconProps extends HTMLAttributes<HTMLImageElement> {
+  iconName: string
+  isDark?: boolean
+}
+
+export const Icon = (props: IconProps) => {
+  const { className, iconName, isDark = false, ...rest } = props
+
+  return (
+    <img
+      className={`h-6 ${isDark ? "" : "invert"} ${className}`}
+      src={`icons/${iconName}.svg`}
+      alt={iconName}
+      {...rest}
+    />
+  )
+}
