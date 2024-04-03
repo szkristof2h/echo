2627e80d0168c186916510abd6b3da87961708d0
jenolah <olahjendev@gmail.com>
2024-04-03 13:05:49 +0200

    update goodbye page buttons

--- src/app/goodbye/page.tsx
+++ src/app/goodbye/page.tsx
@@ -6,36 +6,34 @@ export default function Goodbye() {
   return (
     <>
       <Head title="Goodbye" />
-      <Container className="flex flex-col items-start gap-4" title="Goodbye">
-        <p>
-          Understandable, in that case here are some passive aggressive
-          reccomendations in that case:
-        </p>
-
-        <Link
-          href="/welcome/0"
-          className="inline-block cursor-pointer bg-emerald-400 px-4 py-2 text-white hover:underline"
-        >
-          On a second thought...
-        </Link>
-        <Link
-          href="https://facebook.com"
-          className="inline-block cursor-pointer bg-blue-400 px-4 py-2 text-white hover:underline"
-        >
-          Facebook, the site we can trust with our data!
-        </Link>
-        <Link
-          href="https://reddit.com"
-          className="inline-block cursor-pointer bg-orange-400 px-4 py-2 text-white hover:underline"
-        >
-          Reddit, where AI doesn't steal my content
-        </Link>
-        <Link
-          href="https://tiktok.com"
-          className="inline-block cursor-pointer bg-slate-800 px-4 py-2 text-white hover:underline"
-        >
-          TikTok, attention span is overrated!
-        </Link>
+      <Container className="" title="Goodbye">
+        <div className="flex flex-col gap-4">
+          <p>
+            Understandable, in that case here are some passive aggressive
+            reccomendations in that case:
+          </p>
+          <Link href="/welcome/0" className="button-primary">
+            On a second thought...
+          </Link>
+          <Link
+            href="https://facebook.com"
+            className="button-primary bg-blue-600 hover:bg-blue-800 hover:bg-none"
+          >
+            Facebook, the site we can trust with our data!
+          </Link>
+          <Link
+            href="https://reddit.com"
+            className="button-primary bg-orange-400 hover:bg-orange-600 hover:bg-none"
+          >
+            Reddit, where AI doesn't steal my content
+          </Link>
+          <Link
+            href="https://tiktok.com"
+            className="button-primary bg-slate-700 hover:bg-slate-900 hover:bg-none"
+          >
+            TikTok, attention span is overrated!
+          </Link>
+        </div>
       </Container>
     </>
   )
