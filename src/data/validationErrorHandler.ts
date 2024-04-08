10055dc1e2ed3c580bcca3a46d5cba792c3fada1
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-04-08 13:45:51 +0200

    return error message on validation

--- src/data/validationErrorHandler.ts
+++ src/data/validationErrorHandler.ts
@@ -3,11 +3,16 @@ export default function (error: unknown) {
     if (Array.isArray(error.issues)) {
       const issues: unknown[] = error.issues
 
-      issues.forEach((e) => {
-        console.error("--------- VALIDATION ERROR ---------")
-        if (e && typeof e === "object" && "message" in e)
-          console.error(e?.message)
-        if (e && typeof e === "object" && "path" in e) console.error(e?.path)
-      })
+      return issues
+        .map((e) => {
+          console.error("--------- VALIDATION ERROR ---------")
+          if (e && typeof e === "object" && "message" in e) {
+            console.error(e?.message)
+
+            if (typeof e.message === "string") return e.message
+          }
+          if (e && typeof e === "object" && "path" in e) console.error(e?.path)
+        })
+        .filter(Boolean)
     }
 }
