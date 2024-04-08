fbad451d303cff708d7107c8f90055a6c70ed2d8
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-04-08 18:44:24 +0200

    add required prop to textarea

--- src/app/components/textarea.tsx
+++ src/app/components/textarea.tsx
@@ -3,19 +3,21 @@ import type { HTMLAttributes, ReactElement } from "react"
 interface TextareaProps extends HTMLAttributes<HTMLTextAreaElement> {
   name: string
   placeholder?: string
+  required?: boolean
   label?: ReactElement | string
 }
 
 export const Textarea = (props: TextareaProps) => {
-  const { name, onChange, className, label, ...rest } = props
+  const { name, onChange, className, label, required, ...rest } = props
 
   const styledTextarea = (
     <textarea
       name={name}
       onChange={onChange}
-      className={`ring-primary-light h-36 rounded-lg bg-white px-4 py-2 text-slate-600 shadow-lg outline-none ring-inset placeholder:text-slate-300 focus:ring-2 ${
+      className={`h-36 rounded-lg bg-white px-4 py-2 text-slate-600 shadow-lg outline-none ring-inset ring-primary-light placeholder:text-slate-300 focus:ring-2 ${
         className ?? ""
       }`}
+      required={required}
       {...rest}
     />
   )
