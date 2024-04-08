ec920aca0539b5a8dba9362c59ba0a2c1f833667
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-04-08 19:14:37 +0200

    only show error when there's one

--- src/app/components/form-error.tsx
+++ src/app/components/form-error.tsx
@@ -2,7 +2,8 @@ export default function FormError(props: { errors?: (string | undefined)[] }) {
   const { errors } = props
 
   return (
-    errors && (
+    errors &&
+    errors.length > 0 && (
       <ul className="mt-4 rounded-md bg-danger-dark px-4 py-2 text-white">
         {errors.map((error, i) => (
           <li key={i}>{error}</li>
