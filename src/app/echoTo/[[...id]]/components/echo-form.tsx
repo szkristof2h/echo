545883fe0265ee7bb287a52464d3a59443aad17d
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-04-08 13:48:22 +0200

    use form error component in echo form

--- src/app/echoTo/[[...id]]/components/echo-form.tsx
+++ src/app/echoTo/[[...id]]/components/echo-form.tsx
@@ -4,6 +4,7 @@ import { SubmitButton } from "./submit-button"
 import { useFormState } from "react-dom"
 import { Input } from "~/app/components/input"
 import { Textarea } from "~/app/components/textarea"
+import FormError from "~/app/components/form-error"
 
 export default function EchoForm(props: { id?: string; idParent?: number }) {
   const { id, idParent } = props
@@ -17,19 +18,25 @@ export default function EchoForm(props: { id?: string; idParent?: number }) {
   return state?.status === "success" ? (
     <p>Echo posted successfully!</p>
   ) : (
-    <form action={formAction} className="mt-4 flex flex-col items-center gap-4">
-      <div className="flex w-full flex-col gap-y-4">
-        {!idParent && (
-          <Input label="Title" name="title" autoFocus placeholder="Title" />
-        )}
-        <Textarea
-          label="Your post"
-          name="text"
-          autoFocus
-          placeholder="What's on your mind?"
-        />
-      </div>
-      <SubmitButton text="Submit" />
-    </form>
+    <>
+      <form
+        action={formAction}
+        className="mt-4 flex flex-col items-center gap-4"
+      >
+        <div className="flex w-full flex-col gap-y-4">
+          {!idParent && (
+            <Input label="Title" name="title" autoFocus placeholder="Title" />
+          )}
+          <Textarea
+            label="Your post"
+            name="text"
+            autoFocus
+            placeholder="What's on your mind?"
+          />
+        </div>
+        <SubmitButton text="Submit" />
+      </form>
+      <FormError errors={state.errors} />
+    </>
   )
 }
