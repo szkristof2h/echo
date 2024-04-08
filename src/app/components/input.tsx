e812c6dd3d2c21749731905afee2f937e346a31b
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-04-08 18:43:33 +0200

    add required prop to input

--- src/app/components/input.tsx
+++ src/app/components/input.tsx
@@ -3,18 +3,20 @@ import type { HTMLAttributes, ReactElement } from "react"
 interface InputProps extends HTMLAttributes<HTMLInputElement> {
   name: string
   placeholder?: string
+  required?: boolean
   label?: ReactElement | string
 }
 
 export const Input = (props: InputProps) => {
-  const { name, className, label, ...rest } = props
+  const { name, className, label, required, ...rest } = props
 
   const styledInput = (
     <input
       name={name}
-      className={`ring-primary-light rounded-lg bg-white px-4 py-2 text-slate-600 shadow-lg outline-none ring-inset placeholder:text-slate-300 focus:ring-2 ${
+      className={`rounded-lg bg-white px-4 py-2 text-slate-600 shadow-lg outline-none ring-inset ring-primary-light placeholder:text-slate-300 focus:ring-2 ${
         className ?? ""
       }`}
+      required={required}
       {...rest}
     />
   )
