427aec691267ce8c5483ace595bd5637b4ba63b6
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-04-29 23:11:29 +0200

    query topic for post

--- src/app/(feed)/echo/[id]/page.tsx
+++ src/app/(feed)/echo/[id]/page.tsx
@@ -3,6 +3,7 @@ import { getEcho } from "~/data/echos"
 import Post from "../../components/post"
 import Comments from "../../components/comments"
 import { getUser } from "~/data/users"
+import { getTopic } from "~/data/topics"
 
 export default async function Home({ params }: { params: { id?: string } }) {
   const id = params?.id ? parseInt(params?.id, 10) : null
@@ -13,12 +14,19 @@ export default async function Home({ params }: { params: { id?: string } }) {
   const echo = await getEcho(id)
   const postedBy = echo?.idSender ? await getUser(echo?.idSender) : null
   const postedTo = echo?.idReceiver ? await getUser(echo?.idReceiver) : null
+  const topic = echo?.idTopic ? await getTopic(echo?.idTopic) : null
+  const topicName = topic?.text
 
   return (
     <>
       <Head title={echo?.title ?? "..."} />
       {!!echo ? (
-        <Post postedBy={postedBy} postedTo={postedTo} {...echo} />
+        <Post
+          postedBy={postedBy}
+          postedTo={postedTo}
+          topic={topicName}
+          {...echo}
+        />
       ) : (
         "Couldn't load echo"
       )}
