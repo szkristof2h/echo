6c6ba4d69c0021a9381efe055f39c33cbd53f810
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-04-29 22:13:04 +0200

    add topics to schema

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,9 +1,11 @@
 import { connections } from "./connections"
 import { echos } from "./echos"
 import { suggestions } from "./suggestions"
+import { topics } from "./topics"
 
 export default {
   connections,
   echos,
   suggestions,
+  topics,
 }
